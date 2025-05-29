83934d7413fb8becddf7be5435e99a299bc655df
Darkhan <darkhans@gmail.com>
2025-05-30 02:16:18 +0300

    Написание функции для определения даты в модуле widget

--- src/widget.py
+++ src/widget.py
@@ -11,4 +11,7 @@ def mask_account_card(x: str):
                 mask_account_card_ = x[:4] + ' ' + card_number_[:4] + " " + card_number_[4:6] + "**" + " " + "****" + " " + card_number_[12:]
                 return mask_account_card_
 
-print(mask_account_card('Visa 339933933939'))
+
+def get_date(x: str):
+    """Функция для обработки даты"""
+    return x[8:10] + '.' + x[5:7] + '.' + x[:4]
