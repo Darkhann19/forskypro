8856dea51e13ec809ed44ac8daf8a0611cd2a9c3
Darkhan <darkhans@gmail.com>
2025-05-30 02:11:59 +0300

    Написание функции внутри модуля

--- src/widget.py
+++ src/widget.py
@@ -1,1 +1,14 @@
 def mask_account_card(x: str):
+    """Функция, которая умеет обрабатывать информацию как о картах, так и о счетах"""
+    if 'счет' in x.lower():
+        mask_account_card_ = x[:4] + ' ' + '**' + x[-4:]
+        return mask_account_card_
+
+    else:
+        for i, j in zip(x, range(len(x))):
+            if i.isdigit():
+                card_number_ = x[j:]
+                mask_account_card_ = x[:4] + ' ' + card_number_[:4] + " " + card_number_[4:6] + "**" + " " + "****" + " " + card_number_[12:]
+                return mask_account_card_
+
+print(mask_account_card('Visa 339933933939'))
