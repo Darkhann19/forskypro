db0026b8c89985a6c762123f86dbe61441200f8b
Darkhan <darkhans@gmail.com>
2025-05-31 23:52:08 +0300

    добавление функции для сортировки по датам

--- src/processing.py
+++ src/processing.py
@@ -11,3 +11,15 @@ def filter_by_state(transactions: list[dict], state: str = "EXECUTED") -> list[d
         if i.get("state") == state:
             transactions_.append(i)
     return transactions_
+
+
+def sort_by_date(transactions: list[dict], reverse: bool = True) -> list[dict]:
+    """
+       Сортирует список транзакций по дате.
+
+       """
+    return sorted(
+        transactions,
+        key=lambda x: datetime.fromisoformat(x["date"]),
+        reverse=reverse
+    )
